456c353d3fbdd6b27824956aeaaba815b3a26866
jasper_hansen <jasper.hansen@enovetic.de>
2024-08-27 16:36:29 +0200

    More Info

--- pandas_module.py
+++ pandas_module.py
@@ -1,10 +1,19 @@
 import pandas
 
+# Documentation for pandas: https://pandas.pydata.org/docs/
+
 # Pandas has two main data types: Series (1-Dimensional) and DataFrames (2-Dimensional)
 
 data = pandas.read_csv("weather_data.csv")
-print(type(data)) # --> this is a DataFrame - Equivalent to a table.
-print(type(data["temp"])) # --> this is a Series - Equivalent to a list.
+print(type(data))  # --> this is a DataFrame - Equivalent to a table.
+print(type(data["temp"]))  # --> this is a Series - Equivalent to a list.
 
+# I can use different methods to cast the DataFrame to a Dict, Array or whatever.
 data_dict = data.to_dict()
 print(data_dict)
+
+# Coming to the Series:
+
+temp_list = data["temp"].to_list()
+print(temp_list)
+
